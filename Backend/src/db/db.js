33238831da601b4ab022283c53070d24d9997f9b
Saqib Mehmood <saqibbusiness455@gmail.com>
2026-09-06 19:15:44 +0500

    Fix backend deployment and CORS

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -11,6 +11,7 @@ const connectDB = async () => {
     console.log(`MongoDB connected: ${connection.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
+
     throw error;
   }
 };
