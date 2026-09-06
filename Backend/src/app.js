3fa6229ad74ed5c229c6a77b5eb89163e6c067d5
Saqib Mehmood <saqibbusiness455@gmail.com>
2026-09-06 18:01:46 +0500

    Fix backend CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "https://task-management-system-jhee.vercel.app",
     credentials: true,
   }),
 );
